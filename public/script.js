a4da066d4a618f1cfe2da55510836209c3b1a36c
Renzy Antonio <renzy.antonio0112@gmail.com>
2025-06-09 22:56:03 +0800

    added togglestate function for checkboxes w/ the help of chatgpt

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ const deleteBtnEl = document.querySelector("#deletebtn-el");
 const ulEl = document.querySelector("#list-el");
 
 let taskData = [];
-let taskCheckBox = ["images/checkbox.png", "images/checkbox-fill.png"];
+let taskCheckBox = ["images/checkbox.svg", "images/checkbox-fill.svg"];
 
 let getDatafromStorage = JSON.parse(localStorage.getItem("listKey"));
 
@@ -13,13 +13,36 @@ function renderList() {
     let newList = "";
     for (let i = 0; i < taskData.length; i++) {
         newList += `
-        <li class="flex items-center gap-[10px]">
-            <img src="${taskCheckBox[0]}" class="w-[20px] h-[20px]">
-            ${taskData[i]}
+        <li class="flex items-center gap-[10px]" data-index="${i}">
+            <img src="${taskCheckBox[0]}" class="w-[20px] h-[20px]" data-status="unchecked">
+            <span>${taskData[i]}</span>
         </li>
         `
     }
     ulEl.innerHTML = newList;
+
+    const listItems = ulEl.querySelectorAll("li");
+    listItems.forEach(item => {
+        item.addEventListener("click", toggleTaskStatus);
+    })
+}
+
+function toggleTaskStatus(event) {
+    const listItem = event.currentTarget;
+    const checkboxImage = listItem.querySelector("img");
+    const taskText = listItem.querySelector("span");
+
+    if (checkboxImage.getAttribute("data-status") === "unchecked") {
+        checkboxImage.setAttribute("src", taskCheckBox[1]);
+        checkboxImage.setAttribute("data-status", "checked");
+
+        taskText.classList.add("line-through");
+    } else {
+        checkboxImage.setAttribute("src", taskCheckBox[0]);
+        checkboxImage.setAttribute("data-status", "unchecked");
+
+        taskText.classList.remove("line-through");
+    }
 }
 
 addBtnEl.addEventListener("click", function() {
